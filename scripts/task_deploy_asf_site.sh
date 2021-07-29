64f477d73269d4d2b207998a84f17517ff5d5d39
tqchen <tianqi.tchen@gmail.com>
2021-07-29 18:32:14 -0400

    Update ASF site deployment

--- scripts/task_deploy_asf_site.sh
+++ scripts/task_deploy_asf_site.sh
@@ -6,6 +6,7 @@ set -u
 echo "Start to generate and deploy site ..."
 jekyll b
 cp .gitignore .gitignore.bak
+cp .asf.yaml .asf.yaml.bak
 
 # copy new files into the current site
 git fetch
@@ -14,6 +15,7 @@ git checkout -B asf-site origin/asf-site
 # remove all existing files, excluding the docs
 git ls-files | grep -v ^docs| xargs  rm -f
 cp .gitignore.bak .gitignore
+cp .asf.yaml.bak .asf.yaml
 
 cp -rf _site/* .
 DATE=`date`
